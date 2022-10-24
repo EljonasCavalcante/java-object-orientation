5bf8ebbd2b0edd3da79d9a7c8a7bd08cac3ec9dc
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-10-23 22:26:47 -0300

    int end double

---
+++ sinataxe-variavel-e-fluxo/src/TestaPontoFlutuantes.java
@@ -0,0 +1,12 @@
+
+public class TestaPontoFlutuantes {
+	
+	public static void main(String[] args) {
+		
+		double numero =  5.0 / 2;
+		
+		System.out.println("Meu numero e :" + numero);
+		
+	}
+
+}
