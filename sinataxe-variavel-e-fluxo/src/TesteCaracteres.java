3c90f60f7287de92c337b34cfce66dae925a3238
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-10-26 14:45:44 -0300

    feat: create caractere

---
+++ sinataxe-variavel-e-fluxo/src/TesteCaracteres.java
@@ -0,0 +1,12 @@
+
+public class TesteCaracteres {
+	public static void main(String[] main) {
+		// char save a single letter;
+		char letra = 'a';
+		System.out.println(letra);
+		
+		String palavras = "pode se colocado varias palavras";
+		System.out.println(palavras);
+	}
+
+}
