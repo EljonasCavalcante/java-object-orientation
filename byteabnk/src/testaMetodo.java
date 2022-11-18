342861b0a662e85e5d6c7fb8b2e4d063aaf7178b
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-11-17 21:15:21 -0300

    create file transfere

--- byteabnk/src/testaMetodo.java
+++ byteabnk/src/testaMetodo.java
@@ -4,9 +4,19 @@ public class testaMetodo {
 	public static void main(String[] args) {
 		Conta contaDaBianca = new Conta();
 		contaDaBianca.saldo = 100;
-		contaDaBianca.deposita(1300);
+		contaDaBianca.depositar(1300);
 		System.out.println(contaDaBianca.saldo);
-
+		
+		boolean conseguiuRetirar = contaDaBianca.sacar(100);
+		System.out.println(contaDaBianca.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaDoJonas = new Conta();
+		contaDoJonas.depositar(2000);
+		
+		contaDoJonas.transferir(300,contaDaBianca);
+		System.out.println(contaDoJonas.saldo);
+		System.out.println(contaDoJonas);
 	}
 
 }
